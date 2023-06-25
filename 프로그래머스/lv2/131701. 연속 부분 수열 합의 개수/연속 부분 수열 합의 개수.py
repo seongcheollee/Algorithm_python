dcc2175468b37ac603fcd6ca2644073ffbac99cb
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-06-25 17:17:48 +0900

    Update 연속 부분 수열 합의 개수.py

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -6,6 +6,8 @@ def solution(elements):
     
     for i in range(len(elements)//2):
         for j in range(len(elements)//2):
+            # 각 값에서 나올 수 있는 경우의 수를 모두 삽입.
+            # 7 7+9 7+9+1 ~
              res.add(sum(elements[i:i+j+1]))
     
     return len(res)
