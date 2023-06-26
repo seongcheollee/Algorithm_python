bdc921fdff67897e67b8622e3a2a8ddc60643801
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-06-26 20:00:53 +0900

    [level 2] Title: [1차] 캐시, Time: 0.70 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,5 +1,6 @@
 def solution(cacheSize, cities):
-    arr = []
+    
+    cache = []
     cnt = 0
     
     # 도시 이름 하나 가져오기
@@ -7,16 +8,22 @@ def solution(cacheSize, cities):
         # 대소문자 구분없애기
         ct = ct.lower()
         
-        if ct in arr:
-            arr.remove(ct)
-            arr.append(ct)
+        if ct in cache:
+            # 중복된 값이 존재할 시 LRU 에 따라서 현재 값 가장 뒤로 이동 
+            cache.remove(ct)
+            cache.append(ct)
+            # 캐시 hit
             cnt += 1
         else:
-            arr.append(ct)
+            # 중복된 값이 없음
+            cache.append(ct)
+            # 캐시 miss
             cnt += 5
+            if len(cache) > cacheSize:
+                cache.pop(0)
         
-        if len(arr) > cacheSize:
-            arr.pop(0)
-            
+        
+        # 캐시 사이즈 넘으면 가장 사용되지 않은 값 pop
+                    
     
     return cnt
