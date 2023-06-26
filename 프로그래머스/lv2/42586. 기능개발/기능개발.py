dffcee75c0e68ea7bfd1b92af801c704b39ad83f
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-06-26 23:53:22 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,19 +1,25 @@
-from collections import deque
 import math
 
 def solution(progresses, speeds):
     res = []
     temp = []
+    # 각각의 작업이 100을 넘는 값 추출
     for i in range(len(progresses)):
         temp.append(math.ceil((100 - progresses[i]) / speeds[i]))
     
+    # 가장 선두의 작업이 걸리는 일수보다 작은 것들 count
     first = 0
+    cnt = 1
     for i in range(1, len(progresses)):
-        if temp[first] < temp[i]:
-            res.append(i - first)
+        if temp[first] >= temp[i]:
+            cnt += 1
+        else:
             first = i
+            res.append(cnt)
+            cnt = 1
 
-    res.append(len(progresses) - first)
+    # 마지막 작업 추가
+    res.append(cnt)
             
     return res
 
