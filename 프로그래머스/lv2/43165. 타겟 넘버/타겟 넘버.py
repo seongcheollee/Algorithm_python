5691dae58e13a7ba6917920862daab2e68a0e405
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-06-30 14:23:11 +0900

    [level 2] Title: 타겟 넘버, Time: 2.84 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,15 @@
 def solution(numbers, target):
     global cnt
     cnt = 0
+    # 최초 시작
     dfs(0, 0, numbers, target)
+    
     return cnt
 
 def dfs(i, sum_n, numbers, target):
+    # i 현재 인덱스, sum_n 합산 값
     global cnt
+    
     if i == len(numbers):
         if sum_n == target:
             cnt += 1
@@ -13,3 +17,5 @@ def dfs(i, sum_n, numbers, target):
     
     dfs(i+1, sum_n+numbers[i], numbers, target)
     dfs(i+1, sum_n-numbers[i], numbers, target)
+    
+    return
