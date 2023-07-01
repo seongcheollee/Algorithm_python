e4111fed9ba360fcc10c9562f3e6ac3f20a27f36
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-07-01 16:19:40 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,12 +1,15 @@
 def solution(s):
     res = []
-    # s에서 숫자만
+    # 문자열 s를 숫자만 담긴 배열로 변환
     s = s.replace('{', '')
     s = s.split('},')
     for i in range(len(s)):
         s[i] = s[i].replace('}','')
+    # print(s)
+    # 문자열 길이 수에 따라 정렬 : 튜플 값의 순서를 알기 위해서
     s = sorted(s, key=len)
 
+    # 결과 리스트에 중복된 값을 제외하고 순차적으로 값 삽입
     for i in s:
         i = i.split(',')
         for j in i:
