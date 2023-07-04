4d3a34b48053c765a7c59c3719ab3f1df7c371d4
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-07-04 11:49:19 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -3,17 +3,17 @@ def solution(b, y):
     # 사각형의 크기를 통해 나올 수 있는 가로 세로 변수 뽑기
     # (b+y) = 사각형의 총 넓이
     # 나올 수 있는 row, col을 약수 찾기
+    # 제일 작은 사각형 3x3
     for i in range(3,((b+y)//2)+1):
-        # 약수면
+        # i가 약수면
         if (b+y) % i == 0:
-            # 제일 작은 사각형 3x3
             # i = row , (b+y) // i = column
-            if i >= 3 and (b+y)//i >= 3:
-                if i >= (b+y) // i:
-                    res.append([i,(b+y)//i])
+            if i >= (b+y) // i:
+                res.append([i,(b+y)//i])
     print(res)
     # 모든 경우의 수 중 해당하는 것 찾기
     for sq in res:
+        # 사각형 북 남, 사각형의 동 서 이미 포함시킨거 제거(북 남에 해당) 이 b의 개수면
         if sq[0] * 2 + (sq[1] - 2) * 2 == b:
             return [sq[0], sq[1]]
         
