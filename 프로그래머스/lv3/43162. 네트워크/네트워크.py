fbab34b332eb8617815578ecc4e9e8a7718e2486
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-07-05 18:28:19 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,22 +1,33 @@
+# 문제 요약 : 연결된 네트워크 개수 찾기
+# 문제 풀이
+# 1. 전형적인 dfs 문제로 노드들을 방문 하면서 방문배열 연결여부 체크
+# 2. 연결여부 체크 후 해당 노드들을 한 개의 네트워크로 반환
+# 3. 각각의 노드들을 돌 때 이미 다른 것과 연결되어 있을시 return 0 하여 없는 것으로 간주
+
 def solution(n, computers):
-    answer = 0
-    v = [0] * n  # Initialize visited array locally
+    cnt = 0
+    v = [0] * n  # 방문 확인 배열
     
     def dfs(i):
-        # Check if already visited
+        # 방문 여부 체크
         if v[i]:
             return 0
-        # Mark as visited
+        # 방문 표시
         v[i] = 1
 
+        # 반복문 돌면서 갈 수 있는 노드 진입 
         for j in range(n):
+            # 방문할 수 있고, 방문되지 않았던 노드이면
             if computers[i][j] and not v[j]:
+                # 방문
                 dfs(j)
 
+        # 방문 체크 완료후 리턴
         return 1
 
+    # 배열의 노드들 돌면서 dfs 실행
     for i in range(n):
         if dfs(i):
-            answer += 1
+            cnt += 1
 
-    return answer
+    return cnt
