62313d755a211a726fad8c94c6b617ef6e464258
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-07-15 17:13:47 +0900

    [level 2] Title: 주식가격, Time: 37.81 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,21 +1,32 @@
 # 문제 요약 : 특정 인덱스로부터 오름차 순의 길이 카운트
 from collections import deque
+
 def solution(prices):
     res = []
     q = deque(prices)
     
     while q:
+        # 큐에서 비교할 기준 값 뽑기
         k = q.popleft()
         cnt = 0
-        
+        # 기준값이 사라진 큐 반복문 돌면서 가격이 떨어지는지 확인
         for i in q:
             cnt += 1
             if k > i:
                 break
-    
+        
         res.append(cnt)
-            
-    
+                
     return res
 
-print(solution([1,1,1,1]))
+"""
+        처음 구성
+        for i in q:
+            
+            if k <= i:
+                cnt += 1
+        
+        해당 구성의 문제점
+        1. prices 리스트 전체를 순회함.
+        -> 이미 하락이 되었음에도 cnt 값을 체크하는 문제 발생
+"""
