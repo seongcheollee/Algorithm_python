e9ed65f8767bb5688ab3c9fb5249a18e8aed9414
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-08-08 10:19:58 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,18 +1,22 @@
 def solution(n):
     res = ''
     # 0 1 2 인덱스
-    k = [1,2,4]
+    nList = [1,2,4]
     
-    # 3진법 코드
+    # 3진법 활용
     while n > 0:
+        # 0 이 카운트 되지 않기 때문에, n - 1
+        # 즉 124 나라에서 1은 , 3진법의 계산법으로 0과 같음
         n -= 1
-        t = str(k[n % 3])
+        t = str(nList[n % 3])
         n = n // 3
         res += t
-        
+    
+    #앞자리 0 제거
     if n != 0:
         res += str(n)
 
+    # reverse
     res = res[::-1]
     
     return res
