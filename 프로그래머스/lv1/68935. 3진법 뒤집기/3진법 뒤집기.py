00adf40fbff4519967e9dbcb103de918cb518303
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-08-28 22:21:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,15 +1,21 @@
 def solution(n):
     res = 0
     
+    # n -> 3진법
     def nto3(n):
         st = ''
+        # 종료 조건
         while n > 2:
+            # 나머지 더하고 값변환
             st += str(n % 3)
             n //= 3
+        # 마지막 몫 삽입
         st += str(n)
+        # 리버스해서 변환
         return st[::-1]
     
     n = nto3(n)
+    # 지수가 0일 때
     k = 1
     
     for i in n:
