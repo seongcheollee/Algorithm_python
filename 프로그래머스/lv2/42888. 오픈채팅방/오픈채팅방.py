91a29635256679967596c2e883253fd91132b882
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-09-01 10:18:09 +0900

    [level 2] Title: 오픈채팅방, Time: 77.16 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -4,23 +4,30 @@
 
 
 def solution(record):
+
     res = []
+    # 해시 사용
     nickName = {}
+    
+    # 각 record에서 닉네임 변경에 관여하는 것은 "Enter", "Change"
+    # 해당 동작이 수행될 때 마다, uid 키의 값을 갱신
     for r in record:
         temp = r.split(' ')
         if temp[0] != 'Leave':
             nickName[temp[1]] = temp[2]
     
+    # 출력문 리스트 삽입
     for r in record:
         temp = r.split(' ')
         res_str = ''
+        
         if temp[0] ==  'Enter':
             res_str = nickName[temp[1]]+"님이 들어왔습니다."
         elif temp[0] == 'Leave':
             res_str = nickName[temp[1]]+"님이 나갔습니다."
         else:
             continue
+            
         res.append(res_str)        
         
-    
     return res
