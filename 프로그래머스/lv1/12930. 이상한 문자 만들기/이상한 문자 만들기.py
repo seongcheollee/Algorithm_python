0c81f4bc02d5459ebb3d41cc417eabcfa539119c
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-09-01 09:40:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    words = s.split(' ')
+    res = []
+    
+    for word in words:
+        temp = ''
+        for i,t in enumerate(word):
+            if i % 2 == 0:
+                temp += t.upper()
+            else:
+                temp += t.lower()
+
+        res.append(temp)
+            
+    return ' '.join(res)
