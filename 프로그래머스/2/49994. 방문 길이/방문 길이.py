ca281508040f1ff15e7b8c7f767ee8167565613c
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-11-13 16:25:44 +0900

    [level 2] Title: 방문 길이, Time: 0.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -2,36 +2,30 @@
 # 0,0 -> 5,5에서 시작
 from collections import deque
 
-
 def solution(dirs):
-    answer = 0
-    cnt = 0
-    q = deque(dirs)
-    x,y =4,4
-    nx,ny = 0,0
-    v = []
+    # 초기 좌표 값
+    x, y = 0, 0
     
-    while q:
-        n = q.popleft()
-        if n == "U":
-            nx = x
-            ny = y - 1
-        elif n == "D":
-            nx = x
-            ny = y + 1
-        elif n == "R":
-            nx = x + 1
-            ny = y
+    # 방문 길이 집합.
+    # 저장해야하는 구조 (x,y -> nx,ny)
+    # 집합 구조의 특징 : 
+    visited_dirs = set()
+    
+    for d in dirs:
+        if d == 'U' and y < 5:
+            visited_dirs.add(((x,y),(x, y+1)))
+            y += 1
+        elif d == 'D' and y > -5:
+            visited_dirs.add(((x,y-1),(x, y)))
+            y -= 1
+        elif d == 'L' and x > -5:
+            visited_dirs.add(((x-1,y),(x, y)))  
+            x -= 1
+        elif d == 'R' and x < 5:
+            visited_dirs.add(((x,y),(x+1, y)))  
+            x += 1
         else:
-            nx = x -1
-            ny = y
-                
-        if (0<=nx<10) and (0<=ny<10):
-            if [nx, ny] not in v: 
-                cnt += 1
-                v.append([nx,ny])
-        
-            x = nx
-            y = ny
-
-    return cnt + 1
+            continue
+            
+#    print(visited_dirs)
+    return len(visited_dirs)
