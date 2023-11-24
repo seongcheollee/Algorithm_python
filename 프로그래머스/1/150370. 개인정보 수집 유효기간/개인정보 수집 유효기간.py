73379e21458eb8461f3cba10bed24af30a5f330f
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2023-11-24 16:35:32 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,17 +1,22 @@
 def solution(today, terms, privacies):
-    answer = []
+    res = []
     terms_hash = {}
+    
+    # 계약기간 해시테이블 월 -> 일 기준으로
     for t in terms:
         terms_hash[t.split(' ')[0]] = int(t.split(' ')[1]) * 28
-
+    
+    # 현재 년,월,일
     ny, nm, nd = map(int, today.split('.'))
-    i = 1
-    for p in privacies:
+    
+    for i, p in enumerate(privacies):
+        # 수집 일자,약관 종류
         p, t =  p.split(' ')
+        # 수집 일자 년 월 일
         py, pm, pd = map(int, p.split('.'))
         
+        # 총 지난 일자 계산
         if ((ny - py) * 12* 28) + (nm - pm)*28 + nd - pd >= terms_hash[t]:
-            answer.append(i)
-        i += 1
+            res.append(i+1)
     
-    return answer
+    return res
