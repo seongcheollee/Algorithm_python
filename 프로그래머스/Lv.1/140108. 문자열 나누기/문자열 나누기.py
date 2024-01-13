eb4dbdb23b3ddd97f46c7b3ed8fee6048403a5cf
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2024-01-13 13:45:13 +0900

    [level Lv.1] Title: 문자열 나누기, Time: 1.79 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/Lv.1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,30 +1,38 @@
 def solution(s):
-    answer = 0
     res = 0
+
     x = ''
+    temp = ''
+    
     x_c = 0
     x_nc = 0
-    temp = ''    
-    for i, n in enumerate(s):
+    
+    for n in s:
+        # x 삽입
         if not x:
             x = n
+            temp = n
             x_c += 1
-            temp += n
             continue
-            
+        
+        # x랑 같은지 카운트
         if x == n:
             x_c += 1
         else:
             x_nc += 1
         
+        # 현재까지 읽은 문자열
         temp += n 
-
+        
+        # 카운트 비교
         if x_c == x_nc:
             print(temp)
             res += 1
+            # 기준 문자열, 현재까지 읽은 문자열 초기화
             x = ''
             temp= ''
-            
+    
+    # 나머지 문자열 존재 체크
     if temp:
         res+= 1
         
