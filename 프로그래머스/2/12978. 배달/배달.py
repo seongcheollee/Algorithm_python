22cec27d7857abbc7e49a8317c0cd862545726bf
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2024-01-22 00:35:12 +0900

    [level 2] Title: 배달, Time: 0.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,29 +1,40 @@
 import heapq
-from math import inf
-def solution(N, road, K):
 
-    # 그래프 변형 g[방문지점] = [연결된 노드, 거리]
-    g = [[] for _ in range(N+1)]
-    for i,j,w in road:
-        g[i].append([j,w])
-        g[j].append([i,w])
-    
-    queue = [] # heapq 에 넣을 배열 생성
-    visited = [inf] * (N+1) # 방문시 최단 거리를 추가해주기 위한 배열
-    
-    heapq.heappush(queue,[1, 0])  #시작점 삽입, [노드, 거리]
-    visited[1] = 0 # 시작 지점 거리 값 초기화
-    
-    while queue:
-        # 다음 방문할 노드와 거리 추출   
-        node, dist = heapq.heappop(queue)
+def solution(N, road, K):
+    res = 0
+    INF = int(1e9)
+    # N 마을 개수
+    # K 거리 이하만 배달 가능
+    # 배달가능한 마을 카운트 (시작 노드는 1)
+    dist = [INF] * (N+1)
+    g = [[] for i in range(N+1)]
+    for r in road:
+        a,b,c = r
+        g[a].append((b,c))
+        g[b].append((a,c))
         
-        # 다음 방문 노드
-        for n, d in g[node]:
-            # 현재 거리와 추가될 거리 비교
-            if dist + d < visited[n]:
-                visited[n] = dist + d
-                # 다음 탐색을 위한 힙 푸시
-                heapq.heappush(queue, [n, dist + d])
-    print(visited)
-    return len([i for i in visited if i <=K])
+    def dijkstra(s):
+        q = []
+        heapq.heappush(q, (0,s))
+        dist[s] = 0
+        
+        while q:
+            d, c = heapq.heappop(q)
+            if dist[c] < d:
+                continue
+            
+            for i in g[c]:
+                temp = d + i[1]
+                
+                if temp < dist[i[0]]:
+                    dist[i[0]] = temp
+                    heapq.heappush(q, (temp, i[0]))
+            print(q)
+            print(dist[1:])
+    
+    dijkstra(1)
+    for i in dist:
+        if i <= K:
+            res += 1
+    
+    return res
