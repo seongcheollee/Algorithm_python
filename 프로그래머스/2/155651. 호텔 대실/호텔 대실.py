1f6093af88b6ccc456a9bff701ac870f7a5b2a56
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2024-02-08 06:15:54 +0900

    [level 2] Title: 호텔 대실, Time: 1.85 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -4,33 +4,39 @@ def solution(book_time):
     rooms = []
     room_cnt = 0
     
-#     for b in book_time:
-#         for i in range(2):
-#             if i == 1:
-#                 b[i] = int(b[i].replace(":",""))
-#             else:
-#                 b[i] = int(b[i].replace(":",""))
-                
+    # # 10분 후 계산 했을 경우 분 단위가 59넘어가면 시간 증가해야 하고 분은 오버된 수치
+    # for b in book_time:
+    #     for i in range(2):
+    #         if i == 1:
+    #             b[i] = int(b[i].replace(":",""))+10                
+    #         else:
+    #             b[i] = int(b[i].replace(":",""))
+    
+    # 위 해결을 위해 분단위로 재구성
     for b in book_time:
         for i in range(2):
-            b[i] = int(b[i].split(":")[0]) * 60 + int(b[i].split(":")[1])
-            
+            if i == 1:
+                b[i] = int(b[i].split(":")[0]) * 60 + int(b[i].split(":")[1]) + 10
+            else:
+                b[i] = int(b[i].split(":")[0]) * 60 + int(b[i].split(":")[1])
+    
+    # 입장 시간이 빠른 순으로 정렬            
     book_time.sort()
 
+    
     for s, e in book_time:
+        # 룸에 손님이 하나도 없으면
         if not rooms:
             heappush(rooms,e)
             room_cnt += 1
             continue
+        # 가장 빨리 나가는 손님보다 다음 입실자가 같거나 늦게 들어오면
         if rooms[0] <= s:
             heappop(rooms)
         else:
-            room_cnt += 1
-        heappush(rooms,e+10)
+            room_cnt += 1 # 그렇지 않은 경우 룸 개수 증가
+        heappush(rooms,e)
     
     return room_cnt
 
-book_time = [["10:00", "10:20"], ["09:00", "09:20"], ["09:20", "09:40"], ["09:40", "10:00"]]
 
-
-print(solution(book_time))
