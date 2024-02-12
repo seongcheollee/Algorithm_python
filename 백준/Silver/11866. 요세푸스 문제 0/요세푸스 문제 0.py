2d913fce920d4740358118c9c9a95195b2f56927
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2024-02-13 02:25:06 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -4,19 +4,13 @@ input = sys.stdin.readline
 n, k = map(int, input().split(' '))
 
 nums = [i for i in range(1,n+1)]
-i = k - 1
+i = 0
 res = []
 
-if len(nums) == 1:
-    res = nums
-else:
-    while nums:
-        res.append(nums.pop(i))
-        n -= 1
-        i = (i + k - 1) % n
-
-        if n == 1:
-            res.append(nums.pop())
+while nums:
+    i = (i + k - 1) % n
+    res.append(nums.pop(i))
+    n -= 1
 
 print("<",end="")
 for i in range(len(res)-1):
