d94f244226083a44a668713ec04b843d9f39f2d2
seongcheollee <59824783+seongcheollee@users.noreply.github.com>
2024-02-15 09:54:16 +0900

    [Gold V] Title: AC, Time: 140 ms, Memory: 39404 KB -BaekjoonHub

--- 백준/Gold V/5430. AC/AC.py
+++ 백준/Gold V/5430. AC/AC.py
@@ -1,23 +1,22 @@
 import sys
 from collections import deque
-import json
 input = sys.stdin.readline
 
 T = int(input())
 
 def check_command(command, nums):
-    cnt_r = False
+    cnt_r = 0
     for c in command:
         if c == 'R':
-            cnt_r = not cnt_r
+            cnt_r += 1
         elif c == 'D':
-            if cnt_r:
+            if cnt_r % 2 != 0:
                 nums.pop()
             else:
                 nums.popleft()
 
 
-    if cnt_r:
+    if cnt_r % 2 != 0:
         nums.reverse()
     return ','.join(list(nums))
 
@@ -27,6 +26,7 @@ for i in range(T):
     command = input()
     n = int(input())
     nums = sys.stdin.readline().rstrip()[1:-1]
+
     if len(nums) > 0:
         nums = deque(nums.split(','))
     else:
